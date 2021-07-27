3aa8c40908ba449dda9b89be5f8d30ecab32ca41
Mahutrivedi <86886343+Mahutrivedi@users.noreply.github.com>
2021-07-27 22:05:18 +0530

    Some changes

--- Mnist.py
+++ Mnist.py
@@ -3,7 +3,7 @@
 
 # ### Importing required libraries
 
-# In[3]:
+# In[372]:
 
 
 import numpy as np
@@ -11,19 +11,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# In[4]:
+# In[373]:
 
 
 train_path = r"G:/kaggle/mnist/train.csv"
 
 
-# In[5]:
+# In[374]:
 
 
 df= pd.read_csv(train_path)
 
 
-# In[6]:
+# In[375]:
 
 
 df.head()
@@ -31,26 +31,26 @@ df.head()
 
 # # There are 784 pixels in each image ( 28x28=784) 
 
-# In[7]:
+# In[376]:
 
 
 image = df.iloc[1].drop("label")
 image
 
 
-# In[8]:
+# In[377]:
 
 
 image = np.array(image).reshape(28,28)
 
 
-# In[9]:
+# In[378]:
 
 
 plt.imshow(image)
 
 
-# In[10]:
+# In[379]:
 
 
 # Thats a zero, lets check it on label column if its showing a 0
@@ -58,7 +58,7 @@ plt.imshow(image)
 print(df.iloc[1].label)
 
 
-# In[11]:
+# In[380]:
 
 
 image = df.iloc[3].drop("label")
@@ -71,19 +71,19 @@ print(df.iloc[3].label)
 # 
 # # To have a better accuracy we will do image augmentation by shifting left right up and down the data
 
-# In[12]:
+# In[381]:
 
 
 right_shifted = pd.DataFrame(image).shift(2,axis=1,fill_value=0)
 
 
-# In[13]:
+# In[382]:
 
 
 plt.imshow(right_shifted)
 
 
-# In[14]:
+# In[383]:
 
 
 left_data = []
@@ -93,13 +93,13 @@ down_data=[]
 same_data = [] 
 
 
-# In[15]:
+# In[384]:
 
 
 len(df)
 
 
-# In[16]:
+# In[385]:
 
 
 for row in range(0,len(df)):
@@ -133,7 +133,7 @@ for row in range(0,len(df)):
     same_data.append(image)
 
 
-# In[17]:
+# In[ ]:
 
 
 left_data =np.array(left_data).reshape(len(df),len(df.iloc[0].drop("label")))
@@ -152,7 +152,7 @@ same_data = np.array(same_data).reshape(len(df),len(df.iloc[0].drop("label")))
 same_data = pd.DataFrame(same_data)
 
 
-# In[18]:
+# In[ ]:
 
 
 left_data["label"] = df["label"]
@@ -162,31 +162,31 @@ down_data["label"] = df["label"]
 same_data["label"]=df["label"]
 
 
-# In[19]:
+# In[ ]:
 
 
 left_data.head()
 
 
-# In[20]:
+# In[ ]:
 
 
 data_frames = [same_data,left_data,right_data,up_data,down_data]
 
 
-# In[21]:
+# In[ ]:
 
 
 final_df = pd.concat(data_frames)
 
 
-# In[22]:
+# In[ ]:
 
 
 final_df.head()
 
 
-# In[23]:
+# In[ ]:
 
 
 final_df.shape
@@ -194,7 +194,7 @@ final_df.shape
 
 # #  Now our dataset size is 5x which will make training a bit slow but it will have better accuracy in test phase
 
-# In[24]:
+# In[ ]:
 
 
 x= final_df.drop(["label"],axis=1)
@@ -203,7 +203,7 @@ from sklearn.model_selection import train_test_split
 xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.25,random_state=2021)
 
 
-# In[40]:
+# In[ ]:
 
 
 from sklearn.ensemble import RandomForestClassifier
@@ -213,13 +213,13 @@ forest = RandomForestClassifier(n_estimators=100, criterion='gini', max_depth=20
 
 # # Here i have split the dataset into train and test but it is not necessary because in random forest classifier i will use out of bag evaluation, which works like cross validation score.
 
-# In[41]:
+# In[ ]:
 
 
 forest.fit(xtrain,ytrain)
 
 
-# In[42]:
+# In[ ]:
 
 
 forest.oob_score_
@@ -227,7 +227,7 @@ forest.oob_score_
 
 # # Now saving the model first and then submitting the results in kaggle
 
-# In[56]:
+# In[ ]:
 
 
 parameters = [ 
@@ -237,43 +237,43 @@ parameters = [
 }]
 
 
-# In[46]:
+# In[ ]:
 
 
 from sklearn.model_selection import RandomizedSearchCV
 
 
-# In[57]:
+# In[ ]:
 
 
 randm_search =  RandomizedSearchCV(estimator=forest,param_distributions=parameters,n_iter=5,cv=3,scoring="accuracy")
 
 
-# In[58]:
+# In[ ]:
 
 
 randm_search.fit(x,y)
 
 
-# In[59]:
+# In[ ]:
 
 
 randm_search.best_params_
 
 
-# In[64]:
+# In[ ]:
 
 
 forest = RandomForestClassifier(n_estimators=1000,  criterion='gini', max_depth=50, min_samples_split=8, min_samples_leaf=2,  max_leaf_nodes=10, bootstrap=True, oob_score=False, n_jobs=-1, random_state=2021, verbose=3, )
 
 
-# In[65]:
+# In[ ]:
 
 
 forest.fit(x,y)
 
 
-# In[66]:
+# In[ ]:
 
 
 forest.oob_score_
@@ -283,19 +283,19 @@ forest.oob_score_
 
 # # Now creating final model 
 
-# In[67]:
+# In[ ]:
 
 
 forest = RandomForestClassifier(n_estimators=1000, criterion='gini', max_depth=50, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features='auto', max_leaf_nodes=None, min_impurity_decrease=0.0, min_impurity_split=None, bootstrap=True, oob_score=True, n_jobs=-1, random_state=2021, verbose=3, warm_start=False, class_weight=None, ccp_alpha=0.0, max_samples=None)
 
 
-# In[68]:
+# In[ ]:
 
 
 forest.fit(x,y)
 
 
-# In[71]:
+# In[ ]:
 
 
 forest.oob_score_
@@ -303,65 +303,120 @@ forest.oob_score_
 
 # # The model is not saved because the size of model is big which takes much time for saving the model
 
-# In[72]:
+# In[ ]:
 
 
 test_path = r'G:\kaggle\mnist\test.csv'
 
 
-# In[77]:
+# In[ ]:
 
 
 test_df  = pd.read_csv(test_path)
 test_df.columns= same_data.columns.drop("label")
 
 
-# In[78]:
+# In[ ]:
 
 
 test_df.head()
 
 
-# In[80]:
+# In[ ]:
 
 
 result = forest.predict(test_df)
 
 
-# In[81]:
+# In[ ]:
 
 
 submission_path = r'G:/kaggle/mnist/Submission.csv'
 
 
-# In[86]:
+# In[ ]:
 
 
 submission = pd.read_csv(submission_path)
 
 
-# In[87]:
+# In[ ]:
 
 
 submission.head()
 
 
-# In[88]:
+# In[ ]:
 
 
 submission["Label"] = result
 
 
-# In[89]:
+# In[ ]:
 
 
 submission.head()
 
 
-# In[91]:
+# In[524]:
 
 
 submission.to_csv(r'G:\kaggle\mnist\submission.csv',index=None)
 
 
 # # After uploading it to kaggle competition it got score of 97.37% and rank of 4533.  Not bad :)
+
+# In[525]:
+
+
+manual_test_file_path = r'G:/kaggle/mnist/manual_testing/number.jpg'
+
+
+# In[526]:
+
+
+from PIL import Image
+from numpy import array
+im_1 = Image.open(r'G:/kaggle/mnist/manual_testing/number.jpg')
+img = np.array(im_1)
+
+
+# In[527]:
+
+
+img1 = img[:,:,0]*0.2989 
+img2 = img[:,:,1]*0.5870 
+img3 = img[:,:,2]*0.1140 
+img = img1+img2+img3
+
+
+# In[528]:
+
+
+img
+
+
+# In[531]:
+
+
+plt.imshow(img)
+
+
+# In[532]:
+
+
+import cv2 as cv
+
+
+# In[533]:
+
+
+test_image = (np.array(img)).reshape(1,-1)
+test_image.shape
+
+
+# In[534]:
+
+
+forest.predict(test_image)
+
